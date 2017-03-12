5e28590d3906feca064b0f533f13693d82d89eb0
Outclash <fjlat96@gmail.com>
2017-03-12 11:47:42 +0100

    database

--- cndb/src/com/company/Main.java
+++ cndb/src/com/company/Main.java
@@ -1,9 +1,16 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         mySQLDB connect = new mySQLDB();
-        connect.getData();
+
+//        connect.insertData();
+//        connect.updateData();
+//        connect.deleteData();
+//        connect.getData();
+
     }
 }
--- cndb/src/com/company/mySQLDB.java
+++ cndb/src/com/company/mySQLDB.java
@@ -5,36 +5,52 @@ import java.sql.*;
 /**
  * Created by Francis on 3/11/2017.
  */
+
+/*REFERENCES:
+* https://www.tutorialspoint.com/jdbc/jdbc-sample-code.htm
+* http://zetcode.com/db/mysqljava/
+* http://www.vogella.com/tutorials/MySQLJava/article.html#jdbcdriver
+*/
+
 public class mySQLDB {
 
     // JDBC driver name and database URL
-    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://localhost/test";
+    private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://localhost/test";
 
     //  Database credentials
-    static final String USER = "root";
-    static final String PASS = "";
+    private static final String USER = "root";
+    private static final String PASS = "";
 
-    //java sql
-    public Connection conn;
-    public Statement stmt;
-    public ResultSet res;
+    //java sql variables
+    private Connection conn;
+    private PreparedStatement pstmt;
+    private ResultSet res;
 
+    //set up JDBC_driver
     public mySQLDB(){
         try {
             Class.forName(JDBC_DRIVER);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    //Method to view selected data
+    public void getData(){
+        try {
+            //create connection to database
             conn = DriverManager.getConnection(DB_URL,USER,PASS);
-
-            //create statement
-            stmt = conn.createStatement();
-
-            //create query and Execute a query
+            //create query //!!will be change to get profile of user
             String query = "select * from account";
-            res = stmt.executeQuery(query);
+            //create preparedStatement
+            pstmt = conn.prepareStatement(query);
+            //Execute a query
+            res = pstmt.executeQuery();
 
+            System.out.println("Record from database");
             //Extract data from result set
-            System.out.println("Record from dB");
-            //loop through database
+            //loop through database (cursor)
             while(res.next()){
                 //Retrieve by column name
                 String username = res.getString("username");
@@ -46,22 +62,79 @@ public class mySQLDB {
                 System.out.println("password: " + password);
                 System.out.println("email: " + email);
             }
-            //Clean-up environment
-            res.close();
-            stmt.close();
-            conn.close();
-
         } catch (Exception e) {
-            System.out.println("errr..."+ e);
+            e.printStackTrace();
+            System.out.println(e);
+        } finally {
+            closeRsc();
         }
     }
 
-    public void getData(){
+    //Method to add account and profile
+    public void insertData(){ //need to add parameters for getting input from user
         try {
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+            pstmt = conn.prepareStatement("INSERT INTO account VALUES(?,?,?)");
+            pstmt.setString(1, "kkdso");
+            pstmt.setString(2, "password");
+            pstmt.setString(3, "kkdso@gmail.com");
+            pstmt.executeUpdate();
+            System.out.println("Insert Complete");
+            //make profile here
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            closeRsc();
+        }
+    }
 
+    //Method to delete account and profile
+    public void deleteData(){
+        try {//!! if account is deleted  profile should be deleted too!
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+            pstmt = conn.prepareStatement("DELETE FROM account WHERE username= ?");
+            pstmt.setString(1, "changes");
+            pstmt.executeUpdate();
+            System.out.println("Delete Complete");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            closeRsc();
+        }
+    }
 
+    public void updateData(){
+        try {
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+            //!! change to profile update where ID = id
+            pstmt = conn.prepareStatement("UPDATE account SET username = ?, password = ?, email = ? WHERE username = ?");
+            pstmt.setString(1, "changes");
+            pstmt.setString(2, "password");
+            pstmt.setString(3, "kkdso@gmail.com");
+            pstmt.setString(4, "kkdso");
+            pstmt.executeUpdate();
+            System.out.println("Update Complete");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            closeRsc();
+        }
+    }
+
+    //Close database resources
+    private void closeRsc() {
+        try {
+            if (res != null) {
+                res.close();
+            }
+            if (pstmt != null) {
+                pstmt.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
         } catch (Exception e) {
-            //e.printStackTrace();
+            e.printStackTrace();
             System.out.println(e);
         }
     }
