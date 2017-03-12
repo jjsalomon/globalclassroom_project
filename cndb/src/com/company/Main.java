ecdd681739461a697ec8b8d76b5143c4948a8879
Outclash <fjlat96@gmail.com>
2017-03-12 15:46:50 +0100

    dBver1

--- cndb/src/com/company/Main.java
+++ cndb/src/com/company/Main.java
@@ -6,11 +6,77 @@ public class Main {
 
     public static void main(String[] args) {
         mySQLDB connect = new mySQLDB();
+        String username;
+        String password;
+        String email;
+        int option;
+        boolean loop = true;
+        boolean valid;
+        Scanner scanner = new Scanner(System.in);
 
+        do {
+            System.out.println("Press 1: Login\nPress 2: Register Account\nPress 3: Delete Account\nPress 4: View Profile ");
+            option = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+
+                    System.out.print("Enter username: ");
+                    username = scanner.next();
+                    System.out.print("Enter password: ");
+                    password = scanner.next();
+                    //needs a return so that if login is not possible prints invalid
+                    valid = connect.Login(username,password);
+                    if (valid) {
+                        loop = false;
+                        System.out.println("Welcome " + username);
+                        //!!add the main chess game
+
+                    } else {
+                        System.out.println("Invalid login details");
+                    }
+                    break;
+                case 2:
+                    System.out.print("Enter username: ");
+                    username = scanner.next();
+                    System.out.print("Enter password: ");
+                    password = scanner.next();
+                    System.out.print("Enter email: ");
+                    email = scanner.next();
+                    //returns valid true of false
+                    valid = connect.insertData(username, password, email);
+                    if (valid) {
+                        System.out.println("Register success");
+                    } else {
+                        System.out.println("Register fail");
+                    }
+                    break;
+                case 3:
+                    System.out.print("Enter username: ");
+                    username = scanner.next();
+                    valid = connect.deleteData(username);
+                    if (valid) {
+                        System.out.println("Deletion success");
+                    } else {
+                        System.out.println("Deletion fail or no data found");
+                    }
+                    break;
+                case 4:
+                    System.out.print("Enter username: ");
+                    username = scanner.next();
+                    valid = connect.viewProfile(username);
+                    if (!valid){
+                        System.out.println("There is no such profile");
+                    }
+                    break;
+                default:
+                    System.out.println("Invalid input");
+            }
+
+        }while(loop);
 //        connect.insertData();
 //        connect.updateData();
 //        connect.deleteData();
 //        connect.getData();
-
     }
 }
--- cndb/src/com/company/mySQLDB.java
+++ cndb/src/com/company/mySQLDB.java
@@ -36,86 +36,166 @@ public class mySQLDB {
         }
     }
 
-    //Method to view selected data
-    public void getData(){
+    //Method for logging in
+    public boolean Login(String username, String password){
         try {
             //create connection to database
-            conn = DriverManager.getConnection(DB_URL,USER,PASS);
-            //create query //!!will be change to get profile of user
-            String query = "select * from account";
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
             //create preparedStatement
-            pstmt = conn.prepareStatement(query);
-            //Execute a query
+            pstmt = conn.prepareStatement("SELECT * FROM account WHERE username = ? AND password = ?");
+            pstmt.setString(1, username);
+            pstmt.setString(2, password);
             res = pstmt.executeQuery();
-
-            System.out.println("Record from database");
-            //Extract data from result set
-            //loop through database (cursor)
-            while(res.next()){
-                //Retrieve by column name
-                String username = res.getString("username");
-                String password = res.getString("password");
-                String email = res.getString("email");
-
-                //Display values
-                System.out.println("username: " + username);
-                System.out.println("password: " + password);
-                System.out.println("email: " + email);
+            //validation of account
+            if(res.next()) {
+                return true;
+            }
+            else {
+                return false;
             }
         } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println(e);
+            return false;
+        } finally {
+            closeRsc();
+        }
+
+    }
+    //Method to view selected user profile
+    public boolean viewProfile(String username){
+        try {
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+            pstmt = conn.prepareStatement("SELECT * FROM profile WHERE username = ?");
+            pstmt.setString(1, username);
+            res = pstmt.executeQuery();
+
+            //Extract data from result set
+            //loop through database (cursor)
+            if(res.next()) {
+                System.out.println(username + " profile");
+                //Retrieve by column name
+                int ID = res.getInt("userID");
+                int rank = res.getInt("rank");
+                int win = res.getInt("win");
+                int loss = res.getInt("loss");
+                int coins = res.getInt("coins");
+                String rewardID = res.getString("rewardID");
+
+                //Display values
+                System.out.println("UserID: " + ID);
+                System.out.println("Username: " + username);
+                System.out.println("Rank: " + rank);
+                System.out.println("Win: " + win);
+                System.out.println("Loss: " + loss);
+                System.out.println("Coins: " + coins);
+                System.out.println("Skin: " + rewardID);
+
+                return true;
+            }
+            else {
+                return false;
+            }
+        } catch (Exception e) {
+            return false;
         } finally {
             closeRsc();
         }
     }
 
-    //Method to add account and profile
-    public void insertData(){ //need to add parameters for getting input from user
+    //Method to create account and profile
+    public boolean insertData(String username, String password, String email){ //need to add parameters for getting input from user
         try {
             conn = DriverManager.getConnection(DB_URL,USER,PASS);
+            //create account
             pstmt = conn.prepareStatement("INSERT INTO account VALUES(?,?,?)");
-            pstmt.setString(1, "kkdso");
-            pstmt.setString(2, "password");
-            pstmt.setString(3, "kkdso@gmail.com");
-            pstmt.executeUpdate();
-            System.out.println("Insert Complete");
-            //make profile here
+            pstmt.setString(1, username);
+            pstmt.setString(2, password);
+            pstmt.setString(3, email);
+            int row = pstmt.executeUpdate();
+            //validate if registration account succeed return greater than 0
+            if(row > 0){
+                //create profile
+                int rank = 0;
+                int win = 0;
+                int loss = 0;
+                int coins = 0;
+                String rewardID ="";
+                pstmt = conn.prepareStatement("INSERT INTO profile VALUES(default,?,?,?,?,?,?)");
+                pstmt.setInt(1, rank);
+                pstmt.setInt(2, win);
+                pstmt.setInt(3, loss);
+                pstmt.setInt(4, coins);
+                pstmt.setString(5, username);
+                pstmt.setString(6, rewardID);
+                row = pstmt.executeUpdate();
+                //validate profile creation
+                if(row > 0){
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            }
+            else{
+                return false;
+            }
         } catch (SQLException e) {
-            e.printStackTrace();
+            return false;
         } finally {
             closeRsc();
         }
     }
 
     //Method to delete account and profile
-    public void deleteData(){
+    public boolean deleteData(String username){
         try {//!! if account is deleted  profile should be deleted too!
             conn = DriverManager.getConnection(DB_URL,USER,PASS);
-            pstmt = conn.prepareStatement("DELETE FROM account WHERE username= ?");
-            pstmt.setString(1, "changes");
-            pstmt.executeUpdate();
-            System.out.println("Delete Complete");
+
+            //deletes profile
+            pstmt = conn.prepareStatement("DELETE FROM profile WHERE username = ?");
+            pstmt.setString(1, username);
+            int row = pstmt.executeUpdate();
+            if(row > 0){
+                //deletes account
+                pstmt = conn.prepareStatement("DELETE FROM account WHERE username= ?");
+                pstmt.setString(1, username);
+                row = pstmt.executeUpdate();
+                if(row > 0){
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+            else{
+                return false;
+            }
         } catch (SQLException e) {
-            e.printStackTrace();
+            return false;
         } finally {
             closeRsc();
         }
     }
 
-    public void updateData(){
+    //Method to update profile
+    public boolean updateData(){ //
         try {
             conn = DriverManager.getConnection(DB_URL,USER,PASS);
-            //!! change to profile update where ID = id
+            //!! change to profile update where username = username
             pstmt = conn.prepareStatement("UPDATE account SET username = ?, password = ?, email = ? WHERE username = ?");
             pstmt.setString(1, "changes");
             pstmt.setString(2, "password");
             pstmt.setString(3, "kkdso@gmail.com");
             pstmt.setString(4, "kkdso");
-            pstmt.executeUpdate();
-            System.out.println("Update Complete");
+            int row = pstmt.executeUpdate();
+            if(row > 0){
+                return true;
+            }
+            else{
+                return false;
+            }
+            //System.out.println("Update Complete");
         } catch (SQLException e) {
-            e.printStackTrace();
+            return false;
         } finally {
             closeRsc();
         }
