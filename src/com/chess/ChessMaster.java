4ff061776efcf7e3fb14e9a21f1ace24f3b79810
azuraiii <c14429822@mydit.ie>
2017-03-13 03:58:56 +0000

    Added GUI component

---
+++ src/com/chess/ChessMaster.java
@@ -0,0 +1,20 @@
+package com.chess;
+
+import com.chess.engine.board.Board;
+import com.chess.gui.Table;
+
+/**
+ * Created by azkei on 04/03/2017.
+ */
+public class ChessMaster {
+
+    public static void main(String[] args){
+	
+
+        //Board board = Board.createStandardBoard();
+
+       // System.out.println(board);
+
+        Table table = new Table();
+    }
+}
